dcd532decc86ae77cd1c474c894754b5a796538b
Enzoerb <33969883+Enzoerb@users.noreply.github.com>
2019-10-24 14:57:13 -0300

    Arrumadas incertezas da tabela 2
    também foi adicionado o print da tabela 2 e um melhor plot do gráfico, adicionando os valores de incerteza aos pontos experimentais no eixo y e uma legenda

--- Relatorio_2_MUV.py
+++ Relatorio_2_MUV.py
@@ -18,16 +18,22 @@ v = [media_L[i]/media_dt[i] for i in range(4)]
 incerteza_v = [v[i] * math.sqrt((incerteza_L[i]/media_L[i])**2 + (incerteza_dt[i]/ media_dt[i])**2) for i in range(4)]
 
 #Aplicando Incerteza e Criando Tabela
-incerteza_L = [round(incerteza_L[i], 0) for i in range(4)]
+incerteza_L = [round(incerteza_L[i], 2) for i in range(4)]
+incerteza_Y = [0.0, incerteza_L[0], round(math.sqrt(incerteza_L[0]**2 + incerteza_L[1]**2), 2), round(math.sqrt(incerteza_L[0]**2 + incerteza_L[1]**2 + incerteza_L[2]**2), 2), round(math.sqrt(incerteza_L[0]**2 + incerteza_L[1]**2 + incerteza_L[2]**2 + incerteza_L[3] ** 2), 2)]
+
 media_L = [round(media_L[i], 0) for i in range(4)]
 Y = [0.0, round(media_L[0], 0), round(media_L[0] + media_L[1], 0), round(media_L[0] + media_L[1] + media_L[2], 0), round(media_L[0] + media_L[1] + media_L[2] + media_L[3], 0)] 
+
 incerteza_dt = [round(incerteza_dt[i], 2) for i in range(4)]
+incerteza_X = [0.0, incerteza_dt[0], round(math.sqrt(incerteza_dt[0]**2 + incerteza_dt[1]**2), 2), round(math.sqrt(incerteza_dt[0]**2 + incerteza_dt[1]**2 + incerteza_dt[2]**2), 2), round(math.sqrt(incerteza_dt[0]**2 + incerteza_dt[1]**2 + incerteza_dt[2]**2 + incerteza_dt[3] ** 2), 2)]
+
 media_dt = [round(media_dt[i], 2) for i in range(4)]
 X = [0.0, round(media_dt[0], 2), round(media_dt[0] + media_dt[1], 2), round(media_dt[0] + media_dt[1] + media_dt[2], 2), round(media_dt[0] + media_dt[1] + media_dt[2] + media_dt[3], 2)]
+
 incerteza_v = [round(incerteza_v[i], 0) for i in range(4)]
 v = [round(v[i], 0) for i in range(4)]
 
-tabela = [Y, [0.0] + incerteza_L, X, [0.0] + incerteza_dt]
+tabela2 = [Y, incerteza_Y, X, incerteza_X]
 
 #Criando Somatórias
 somatoria_y = 0
@@ -37,12 +43,12 @@ somatoria_xy = 0
 somatoria_x2 = 0
 
 #Executando Somatórias
-for i in range(0, len(media_L)):
-    somatoria_y += Y[i]/(incerteza_L[i]**2)
-    somatoria_x += X[i]/(incerteza_L[i]**2)
-    somatoria_1 += 1/(incerteza_L[i]**2)
-    somatoria_xy += (Y[i] * X[i])/(incerteza_L[i]**2)
-    somatoria_x2 += (X[i]**2)/(incerteza_L[i]**2)
+for i in range(1, len(X)):
+    somatoria_y += Y[i]/(incerteza_Y[i]**2)
+    somatoria_x += X[i]/(incerteza_Y[i]**2)
+    somatoria_1 += 1/(incerteza_Y[i]**2)
+    somatoria_xy += (Y[i] * X[i])/(incerteza_Y[i]**2)
+    somatoria_x2 += (X[i]**2)/(incerteza_Y[i]**2)
 
 #Arrumando eros de conversões das somatórias
 somatoria_y = round(somatoria_y , 10)
@@ -58,17 +64,37 @@ b = (somatoria_y - (a * somatoria_x))/(somatoria_1)
 #Calculando <X>, <X²>, <incerteza²>
 chaves_x = somatoria_x / somatoria_1
 chaves_x2 = somatoria_x2 / somatoria_1
-chaves_incerteza = len(media_L) / somatoria_1
+chaves_incerteza = len(X) / somatoria_1
 
 #Calculando Delta_A e Delta_B
 delta_a = (1 / math.sqrt(len(X))) * math.sqrt((chaves_incerteza / (chaves_x2 - (chaves_x**2))))
 delta_b = (1 / math.sqrt(len(X))) * math.sqrt(((chaves_incerteza * chaves_x2) / (chaves_x2 - (chaves_x**2))))
 
+#Estimando valores
+valores_estimados = [(j, round(a*j + b, 2)) for j in range(5)]
+
+#Print Tabela 2
+print(f"""
+ ______________________________________________
+|Sensor |X(cm) |incerteza X |t(s) |incerteza t | 
+|------ |------|------------|-----|------------|
+|1      |{Y[0]}   |{incerteza_Y[0]}         |{X[0]}  |{incerteza_X[0]}         |  
+|------ |------|------------|-----|------------|  
+|2      |{Y[1]}  |{incerteza_Y[1]}        |{X[1]}  |{incerteza_X[1]}        |
+|------ |------|------------|-----|------------|
+|3      |{Y[2]}  |{incerteza_Y[2]}        |{X[2]} |{incerteza_X[2]}        |
+|------ |------|------------|-----|------------|
+|4      |{Y[3]}  |{incerteza_Y[3]}         |{X[3]} |{incerteza_X[3]}        |
+|------ |------|------------|-----|------------|
+|5      |{Y[4]}  |{incerteza_Y[4]}        |{X[4]}  |{incerteza_X[4]}        |
+|_______|______|____________|_____|____________|
+""")
+
 #Exibindo dados
 print(f"""
 X: {X}
 Y: {Y}
-Valores Estimados: {[round(a*j + b, 2) for j in range(5)]}
+Valores Estimados: {valores_estimados}
 a = {a}
 b = {b}
 delta_a = {delta_a}
@@ -76,8 +102,13 @@ delta_b = {delta_b}
 """)
 
 #Plotando Gráfico
-plt.scatter(X, Y, color = 'red')
-plt.plot(X, [a*j + b for j in X], 'blue')
+plt.scatter(X, Y, s = 20, color = 'blue', label = 'pontos experimentais')
+plt.scatter(X, [Y[i] + incerteza_Y[i] for i in range(5)], s = 2, color = 'green', label = 'incertezas experimentais')
+plt.scatter(X, [Y[i] - incerteza_Y[i] for i in range(5)], s = 2,  color = 'green')
+for i in range(len(X)):
+    plt.plot([X[i], X[i]], [Y[i] - incerteza_Y[i], Y[i] + incerteza_Y[i]], color = 'green')
+plt.plot(X, [a*j + b for j in X], 'red', label = 'best fitting line')
+plt.legend()
 plt.ylabel("x(cm)")
 plt.xlabel("Delta t(s)")
 plt.title("X vs Delta t (MUV)")
